e0a6a97bef751d77a276e8acf6e50f39c53cfc1d
WilliamThompa <110089072+WilliamThompa@users.noreply.github.com>
2024-05-15 23:37:45 +1200

    Different solution to hopefully fix refreshing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,11 +13,11 @@ function Navbar() { //Navbar sits in an array
         </div>
 
         <ul className='hidden md:flex' > {/*Tells the nav bar to hide when below 768px*/}
-            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/">Home</Link></li>
-            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/about">About</Link></li>
-            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/projects">Projects</Link></li>
-            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/experience">Experience</Link></li>
-            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/contact">Contact</Link></li>
+            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/WilliamThompsonPortfolio/">Home</Link></li>
+            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/WilliamThompsonPortfolio/about">About</Link></li>
+            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/WilliamThompsonPortfolio/projects">Projects</Link></li>
+            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/WilliamThompsonPortfolio/experience">Experience</Link></li>
+            <li className='px-4 cursor-pointer font-bold font-nav text-xl hover:scale-125 hover:text-yellow-400 duration-500 capitalize text-white'><Link to="/WilliamThompsonPortfolio/contact">Contact</Link></li>
         </ul>
 
         <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-white md:hidden'> {/*Tells the hamburger navbar when to appear*/}
@@ -26,11 +26,11 @@ function Navbar() { //Navbar sits in an array
 
         {nav && ( //Using the useState tells the navbar to appear when the hamburger icon is clicked
             <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-slate-900 via-slate-900 to-blue-950 text-white'>
-                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/'>Home</Link></li>
-                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/about'>About</Link></li>
-                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/projects'>Projects</Link></li>
-                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/experience'>Experience</Link></li>
-                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/contact'>Contact</Link></li>
+                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/WilliamThompsonPortfolio/'>Home</Link></li>
+                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/WilliamThompsonPortfolio/about'>About</Link></li>
+                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/WilliamThompsonPortfolio/projects'>Projects</Link></li>
+                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/WilliamThompsonPortfolio/experience'>Experience</Link></li>
+                <li className='px-4 cursor-pointer capitalize py-6 text-4xl'><Link onClick={() => setNav(!nav)} to='/WilliamThompsonPortfolio/contact'>Contact</Link></li>
             </ul>
     )}
     </div>
