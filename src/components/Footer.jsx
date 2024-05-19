b81bd851b2fcc9eca034afd2600e626946752083
WilliamThompa <110089072+WilliamThompa@users.noreply.github.com>
2024-05-20 09:15:38 +1200

    Changed Footer CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='bg-blue-950 text-white pl-2 pt-2 w-full flex'>
-        <h1>Copyright © 2024 William Thompson</h1>
+    <div className="bg-blue-950 text-white pl-2 pt-7 w-full flex">
+      <h1>Copyright © 2024 William Thompson</h1>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
