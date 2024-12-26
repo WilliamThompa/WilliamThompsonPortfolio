52b08c6a5021d3abd3ad961bcba060d740dd6d84
WilliamThompa <110089072+WilliamThompa@users.noreply.github.com>
2024-12-27 00:43:38 +1300

    Construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
-import Navbar from "./components/Navbar"
 import ContactLinks from "./components/ContactLinks"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Experience from "./components/Experience"
-import Contact from "./components/Contact"
-import LoaderSimple from "./components/LoaderSimple"
 
 function App() {
   return (
     <div>
-      <LoaderSimple />
-      <Navbar />
-      <ContactLinks />
-      <Routes>
-          <Route path="/WilliamThompsonPortfolio/" element={ <Home/> } />
-          <Route path="/WilliamThompsonPortfolio/about" element={ <About/> } />
-          <Route path="/WilliamThompsonPortfolio/projects" element={ <Projects/> } />
-          <Route path="/WilliamThompsonPortfolio/experience" element={ <Experience/> } />
-          <Route path="/WilliamThompsonPortfolio/contact" element={ <Contact/> } />
-      </Routes>
+      <ContactLinks/>
+      <Home/>
     </div>
   )  
   
